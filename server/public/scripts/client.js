d857fe38ecd7f493c7032ee5aa7d984fca140093
Tina Tram <tinatram07@gmail.com>
2022-11-30 13:41:24 -0600

    wrote ajax POST request

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -8,16 +8,20 @@ function readyNow() {
     $('#minus-btn').on('click', storeOperation);
     $('#multiply-btn').on('click', storeOperation);
     $('#divide-btn').on('click', storeOperation);
-    $('#equal-btn').on('click', calculateNumber);
-}
+    //$('#equal-btn').on('click', calculateNumber);
+    $('#equal-btn').on('click', sendCalculation);
+} // end readyNow
 
+// store operator being used in an array to determine which conditional is executed
+// alert saying user cannot use multiple operators at once
 function storeOperation() {
     calculationArray.push(this.innerText);
     if (calculationArray.length > 1) {
         alert('Cannot do multiple operations at once');
     }
-}
+} // end storeOperation
 
+// get user input
 function calculateNumber() {
     let firstNumber = Number($('#first-input').val());
     let secondNumber = Number($('#second-input').val());
@@ -37,6 +41,22 @@ function calculateNumber() {
         let result = firstNumber / secondNumber;
         console.log(result);
     }
+    objectToSend = {value: result}
     
+} // end calculateNumber
+
+// send object to server
+function sendCalculation() {
+    calculateNumber();
+    $.ajax({
+        method: 'POST',
+        url: '/calculations',
+        data: objectToSend,
+    }).then(function(response) {
+        console.log('back from POST', response);
+    }).catch(function(error) {
+        console.log(error);
+        alert('error posting message')
+    }) // end ajax
 }
 
--- server/server.js
+++ server/server.js
@@ -5,6 +5,14 @@ const port = 5050;
 
 app.use(express.static('server/public'));
 
+// POST request - allows us to parse data from front-end
+app.use(express.urlencoded());
+
 app.listen(port, () => {
     console.log('listening on port', port);
 });
+
+app.post('/calculations', (req, res) => {
+    console.log('in /calculations POST', req.body);
+    res.send('ribbet');
+}) // end /calculations POST
