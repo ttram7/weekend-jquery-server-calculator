f33223d1912e18f2155db07fb3b6e96444b7fe50
Tina Tram <tinatram07@gmail.com>
2022-11-30 13:15:38 -0600

    wrote storeOperation function to store operator being used and conditionals to console log the calculation result

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,42 @@
 $(readyNow);
 
+// array that stores type of operator clicked on
+let calculationArray = []
+
 function readyNow() {
+    $('#plus-btn').on('click', storeOperation);
+    $('#minus-btn').on('click', storeOperation);
+    $('#multiply-btn').on('click', storeOperation);
+    $('#divide-btn').on('click', storeOperation);
+    $('#equal-btn').on('click', calculateNumber);
+}
+
+function storeOperation() {
+    calculationArray.push(this.innerText);
+    if (calculationArray.length > 1) {
+        alert('Cannot do multiple operations at once');
+    }
+}
+
+function calculateNumber() {
+    let firstNumber = Number($('#first-input').val());
+    let secondNumber = Number($('#second-input').val());
+    if (calculationArray[0]=== '+') {
+        let result = firstNumber + secondNumber;
+        console.log(result);
+    }
+    if (calculationArray[0]=== '-') {
+        let result = firstNumber - secondNumber;
+        console.log(result);
+    }
+    if (calculationArray[0]=== '*') {
+        let result = firstNumber * secondNumber;
+        console.log(result);
+    }
+    if (calculationArray[0]=== '/') {
+        let result = firstNumber / secondNumber;
+        console.log(result);
+    }
     
 }
+
