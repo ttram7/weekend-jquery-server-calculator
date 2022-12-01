8cd501e482689a807dbdf20aba03247bbcd6231a
Tina Tram <tinatram07@gmail.com>
2022-12-01 12:43:17 -0600

    added server-side logic for rest of operations (-, *, /)

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,8 +1,9 @@
 $(readyNow);
-console.log('test', {testNum: 4});
-// array that stores type of operator clicked on
-let operatorArray = []
 
+// array that stores type of operator clicked on
+let operatorArray = [];
+
+// functions will execute when its respective button is clicked on
 function readyNow() {
     $('#plus-btn').on('click', storeOperation);
     $('#minus-btn').on('click', storeOperation);
@@ -10,11 +11,10 @@ function readyNow() {
     $('#divide-btn').on('click', storeOperation);
     $('#equal-btn').on('click', sendCalculation);
     $('#clear-btn').on('click', clearInputs);
-    //$('#equal-btn').on('click', calculateNumber);
 } // end readyNow
 
-// store operator being used in an array to determine which conditional is executed
-// alert saying user cannot use multiple operators at once
+// store operator that was clicked on in an array to be used for calculation on server-side
+// alert appears when user does not clear inputs to avoid using multiple operators at once
 function storeOperation() {
     if (operatorArray.length === 1) {
         alert('Cannot do multiple operations at once. Please hit "C" before entering a new calculation.');
@@ -23,69 +23,47 @@ function storeOperation() {
     }
 } // end storeOperation
 
+// input fields will clear when user clicks "C" button
+// operator in operatorArray is removed to reset for the next calculation
 function clearInputs() {
     $('#first-input').val('');
     $('#second-input').val('');
     operatorArray.pop();
 }
 
-// get user input
-/*function calculateNumber() {
-    let firstNumber = Number($('#first-input').val());
-    let secondNumber = Number($('#second-input').val());
-    if (calculationArray[0]=== '+') {
-        let result = firstNumber + secondNumber;
-        console.log(result);
-    }
-    if (calculationArray[0]=== '-') {
-        let result = firstNumber - secondNumber;
-        console.log(result);
-    }
-    if (calculationArray[0]=== '*') {
-        let result = firstNumber * secondNumber;
-        console.log(result);
-    }
-    if (calculationArray[0]=== '/') {
-        let result = firstNumber / secondNumber;
-        console.log(result);
-    }
-    objectToSend = {value: result}
-    
-} // end calculateNumber
-*/
-// send object to server
+// send inputs bundled in an object to server
 function sendCalculation() {
-    //firstNum and secondNum are still strings here;
+    // NOTE: firstNum and secondNum are still strings here;
     $.ajax({
         method: 'POST',
         url: '/calculations',
         data: {
             firstNum: Number($('#first-input').val()),
             secondNum: Number($('#second-input').val()),
-            testNum: 4,
             operator: operatorArray[0]
         }
     }).then(function(response) {
-        console.log('back from POST - console log', response);
-        console.log(operatorArray);
+        console.log('back from POST - success message:', response);
+        console.log('Operator being used:', operatorArray);
         getCalculation();
     }).catch(function(error) {
         console.log(error);
-        alert('error posting message')
+        alert('error posting message');
     }) // end ajax
-}
+} // end sendCalculation
 
+// gets calculationArray
 function getCalculation() {
     $.ajax({
         method: 'GET',
         url: '/calculations'
     }).then(function(response){
-        console.log('back from GET - console log', response);
+        console.log('back from GET - calculationArray', response);
         appendToDom(response);
-    })
-}
+    }) // end ajax
+} // end getCalculation
 
-// append list of calculations to DOM
+// append result and list of calculations to DOM
 function appendToDom(array) {
     $('#result').empty();
     $('#history-list').empty();
@@ -93,8 +71,7 @@ function appendToDom(array) {
         $('#result').text(object.result);
         $('#history-list').append(`
         <li>${object.firstNum} ${object.operator} ${object.secondNum} = ${object.result}</li>
-    
         `)
     }
-}
+} // end appendToDom
 
--- server/server.js
+++ server/server.js
@@ -8,48 +8,36 @@ app.use(express.static('server/public'));
 // POST request - allows us to parse data from front-end
 app.use(express.urlencoded());
 
-// to store operator used in calculation
-//let operatorArray = []
+// stores all calculations
 let calculationArray = [];
 
 app.post('/calculations', (req, res) => {
     const calculation = req.body;
 
-    //do calculation here
+    //do calculation here, number conversion works here
     if (calculation.operator === '+') {
         calculation.result = Number(calculation.firstNum) + Number(calculation.secondNum);
     }
+    if (calculation.operator === '-') {
+        calculation.result = Number(calculation.firstNum) - Number(calculation.secondNum);
+    }
+    if (calculation.operator === '*') {
+        calculation.result = Number(calculation.firstNum) * Number(calculation.secondNum);
+    }
+    if (calculation.operator === '/') {
+        calculation.result = Number(calculation.firstNum) / Number(calculation.secondNum);
+    }
+    
     console.log('in /calculations POST on server', calculation);
     calculationArray.push(calculation);
-    //send back success
+    // send back success
     res.sendStatus(201);
 }) // end /calculations POST
 
 //GET request after the POST to get the actual calculation
-
 app.get('/calculations', (req, res) => {
-    //req is an incoming message object?
-    // do calculations here
-    res.send(calculationArray)
-    // if (calculationArray[0]=== '+') {
-    //     let result = firstNumber + secondNumber;
-    //     console.log(result);
-    // }
-    // if (calculationArray[0]=== '-') {
-    //     let result = firstNumber - secondNumber;
-    //     console.log(result);
-    // }
-    // if (calculationArray[0]=== '*') {
-    //     let result = firstNumber * secondNumber;
-    //     console.log(result);
-    // }
-    // if (calculationArray[0]=== '/') {
-    //     let result = firstNumber / secondNumber;
-    //     console.log(result);
-    // }
-    //res.send(result);
-    //res.send(calculationArray);
-})
+    res.send(calculationArray);
+}) // end /calculations GET
 
 app.listen(port, () => {
     console.log('listening on port', port);
